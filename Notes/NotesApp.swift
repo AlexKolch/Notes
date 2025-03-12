7285b192a2536e42be461fd992be5f6f29a32cfd
Alex Kolch <alexkolch.ios@gmail.com>
2025-03-12 16:56:02 +0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Алексей Колыченков on 12.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
