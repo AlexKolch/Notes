f11b6c2c90bc4f9c9a869da0416cf0ecaf5bda72
Alex Kolch <alexkolch.ios@gmail.com>
2025-03-15 12:13:19 +0300

    search logic done

--- Notes/Services/DataManager.swift
+++ Notes/Services/DataManager.swift
@@ -6,17 +6,20 @@
 //
 
 import Foundation
+import Combine
 
 protocol DataManagerProtocol {
     var networkManager: Networking {get set}
     var dataBaseManager: DataBaseManagerProtocol {get set}
     func getAllNotes(_ completion: @escaping ([Todo]) -> Void)
     func updateNote(note: Todo, _ completion: @escaping ([Todo]) -> Void)
+    var fetchedNotes: [Todo] {get}
 }
 
 final class DataManager: DataManagerProtocol {
     var networkManager: Networking
     var dataBaseManager: DataBaseManagerProtocol
+    @Published var fetchedNotes: [Todo] = []
     
     init(networkManager: Networking = NetworkManager(), dataBaseManager: DataBaseManagerProtocol = DataBaseManager()) {
         self.networkManager = networkManager
@@ -27,6 +30,7 @@ final class DataManager: DataManagerProtocol {
         dataBaseManager.getAllNotes { notes in
             if !notes.isEmpty {
                 completion(notes)
+                self.fetchedNotes = notes
                 print("Запрос к БД")
             } else {
                 print("Запрос к серверу")
@@ -38,6 +42,7 @@ final class DataManager: DataManagerProtocol {
                         print("Данные от сервера загружены")
                             self.dataBaseManager.saveAll(notes: notes) {
                                 completion(notes) //отправляем заметки после сохранения
+                                self.fetchedNotes = notes
                             }
                     }
                 }
--- Notes/ViewModel/NotesListViewModel.swift
+++ Notes/ViewModel/NotesListViewModel.swift
@@ -7,20 +7,22 @@
 
 import Foundation
 import SwiftfulRouting
+import Combine
 //protocol NotesListVMProtocol {
 //    var dbManager: DataManagerProtocol {get set}
 //    var notes: [Todo] {get}
 //}
 
 final class NotesListViewModel: ObservableObject {
-    private let dataManager: DataManagerProtocol
+    private let dataManager: DataManager
     let router: AnyRouter
     @Published var notes: [Todo] = []
     @Published var searchText: String = ""
-    @Published var selectedTodo: Todo? 
+    @Published var selectedTodo: Todo?
+    private var cancellables = Set<AnyCancellable>()
 //    @Published var errorMessage: String?
     
-    init(dataManager: DataManagerProtocol = DataManager(), router: AnyRouter) {
+    init(dataManager: DataManager = DataManager(), router: AnyRouter) {
         self.dataManager = dataManager
         self.router = router
         DispatchQueue.global().async {
@@ -31,6 +33,7 @@ final class NotesListViewModel: ObservableObject {
                 }
             }
         }
+        addSubscibers()
     }
     
     func updateStatusTodo() {
@@ -47,6 +50,29 @@ final class NotesListViewModel: ObservableObject {
         dataManager.updateNote(note: updatedTodo) { notes in
             self.notes = notes
         }
+    }
+    
+    private func filterTodos(inputText: String, todos: [Todo]) -> [Todo] {
+        guard !inputText.isEmpty else {
+            return todos
+        }
+        let searchingLowercasedText = inputText.lowercased()
+        return todos.filter {
+            $0.todo.lowercased().contains(searchingLowercasedText)
+        }
+    }
+    
+    private func addSubscibers() {
+        $searchText
+            .combineLatest(dataManager.$fetchedNotes)
+            .debounce(for: .seconds(0.4), scheduler: DispatchQueue.main)
+            .map { text, todos in
+                self.filterTodos(inputText: text, todos: todos)
+            }
+            .sink { [weak self] returnedTodos in
+                self?.notes = returnedTodos
+            }
+            .store(in: &cancellables)
     }
 
 }
